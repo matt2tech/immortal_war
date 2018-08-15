9fa1b8497c410007b1e050b9cb2041993eac99aa
matt2tech <mlipsey@basecampcodingacademy.org>
2018-08-15 09:30:10 -0500

    some tests so far

---
+++ test_disk.py
@@ -0,0 +1,22 @@
+from disk import *
+from bcca.test import (
+    should_print,
+    fake_file,
+)
+
+
+@fake_file({'save.txt': '''header line
+line
+line
+line
+'''})
+def test_save_read():
+    assert save_read() == 'line\nline\nline\n'
+
+
+def test_save_to_character():
+    contents = 'Matt,Sir,2,2100,300'
+
+    content = save_to_character(contents)
+
+    assert content == ('Matt', 'Sir', 2, 2100, 300)
